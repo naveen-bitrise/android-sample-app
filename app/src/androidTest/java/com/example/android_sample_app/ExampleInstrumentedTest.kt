704fc2859fc78da3b186da3781ef4bdc4cede56f
Naveen Nazimudeen <naveen.nazimudeen@bitrise.io>
2025-05-28 10:13:54 -0400

    added test case

--- app/src/androidTest/java/com/example/android_sample_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android_sample_app/ExampleInstrumentedTest.kt
@@ -24,19 +24,29 @@ class ExampleInstrumentedTest {
     }
 
     @Test
-    fun longRunningTest() {
+    fun oneMinuteRunningTest() {
         // Test that sleeps for 10 minutes
-        println("Starting 10-minute sleep test")
+        println("Starting 1-minute sleep test")
         try {
-            // Sleep for 10 minutes (600,000 milliseconds)
-            Thread.sleep(TimeUnit.MINUTES.toMillis(10))
-            println("10-minute sleep completed")
+            // Sleep for 1 minutes (60,000 milliseconds)
+            Thread.sleep(TimeUnit.MINUTES.toMillis(1))
+            println("1-minute sleep completed")
         } catch (e: InterruptedException) {
             println("Sleep was interrupted: ${e.message}")
             throw e
         }
         // Simple assertion to make sure the test passes after sleeping
         assertTrue(true)
+    }
+
+    @Test
+    fun testGreetingTextDisplayed() {
+        // Launch the MainActivity
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Verify that the text "Hello; Bitrise!" is displayed
+        onView(withText("Hello; Bitrise!"))
+            .check(matches(isDisplayed()))
     }
 
 
